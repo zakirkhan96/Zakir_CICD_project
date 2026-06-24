fb7ec026a28aef4c782ec79d2dccec3f2f5120bc
Zakir <zk548473@gmail.com>
2026-06-24 14:46:52 +0500

    Set app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 http.createServer((req, res) => {
  res.writeHead(200);
- res.end('Hello from Jenkins Pipeline! v1\n');
- res.end('Actually it the version 10.');
+ res.end('Hello from Jenkins Pipeline! v1\n Actually it V10.');
 }).listen(3000, () => console.log('Running on port 3000'));
 
