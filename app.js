98f2e281a5b1c6071a4ea4aefc1a587affc3b8bd
Zakir <zk548473@gmail.com>
2026-06-24 14:06:55 +0500

    Change version in app,js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const http = require('http');
 http.createServer((req, res) => {
  res.writeHead(200);
  res.end('Hello from Jenkins Pipeline! v1\n');
+ res.end('Actually it the version 10.');
 }).listen(3000, () => console.log('Running on port 3000'));
 
